8bc067aca22c7942b8c3db97326af067927ae01e
Nicolas <nico_8830_3@hotmail.com>
2020-12-26 13:16:42 +0000

    adding requirements_sge.sh

---
+++ requirements_sge.sh
@@ -0,0 +1,16 @@
+apt-get -y install build-essential
+apt-get -y install xfstt t1-xfree86-nonfree ttf-xfree86-nonfree ttf-xfree86-nonfree-syriac xfonts-75dpi xfonts-100dpi
+#apt-get -y install gridengine-common gridengine-client gridengine-exec gridengine-master gridengine-qmon
+apt-get -y install libhwloc-dev
+apt-get -y install libxt-dev llvm-10-dev
+apt-get -y install libmotif-dev
+apt-get -y install libxext-dev
+apt-get -y install libwolfssl-dev
+apt-get -y install libssl-dev
+apt-get -y install libbind-dev
+apt-get -y install libpam0g-dev
+apt-get -y install libhwloc-dev
+apt-get -y install freebsd-glue
+apt-get -y install libtirpc-dev
+apt-get -y install cmake cmake-doc ninja-build
+
