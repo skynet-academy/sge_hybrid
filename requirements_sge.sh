f498066119f76cd3bb77f935997315ff39a863db
skynet118 <64376440+skynet118@users.noreply.github.com>
2020-12-29 23:20:39 +0300

    Update requirements_sge.sh
    basic packages to install sun grid engine in LinuxOne

--- requirements_sge.sh
+++ requirements_sge.sh
@@ -1,16 +1,5 @@
-apt-get -y install build-essential
-apt-get -y install xfstt t1-xfree86-nonfree ttf-xfree86-nonfree ttf-xfree86-nonfree-syriac xfonts-75dpi xfonts-100dpi
-#apt-get -y install gridengine-common gridengine-client gridengine-exec gridengine-master gridengine-qmon
-apt-get -y install libhwloc-dev
-apt-get -y install libxt-dev llvm-10-dev
-apt-get -y install libmotif-dev
-apt-get -y install libxext-dev
-apt-get -y install libwolfssl-dev
-apt-get -y install libssl-dev
-apt-get -y install libbind-dev
-apt-get -y install libpam0g-dev
-apt-get -y install libhwloc-dev
-apt-get -y install freebsd-glue
-apt-get -y install libtirpc-dev
-apt-get -y install cmake cmake-doc ninja-build
+apt-get -y install build-essential xfstt t1-xfree86-nonfree ttf-xfree86-nonfree ttf-xfree86-nonfree-syriac xfonts-75dpi xfonts-100dpi libhwloc-dev \
+libxt-dev llvm-10-dev libmotif-dev libxext-dev libwolfssl-dev libssl-dev libbind-dev libpam0g-dev libhwloc-dev freebsd-glue libtirpc-dev cmake cmake-doc ninja-build \
+java maven
 
+#apt-get -y install gridengine-common gridengine-client gridengine-exec gridengine-master gridengine-qmon
